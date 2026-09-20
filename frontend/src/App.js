ba2f222d77adeb0b85d8f3d4cfb6822da19f9ef3
dw8yhmnzb6-cmyk <afrimk@me.com>
2026-09-20 23:48:06 +0200

    Register public Charge warranty verification route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -208,6 +208,7 @@ const ChargeAppPage = lazy(() => import("./pages/ChargeAppPage"));
 const ChargeMerchantDetailPage = lazy(() => import("./pages/ChargeMerchantDetailPage"));
 const ChargeProductDetailPage = lazy(() => import("./pages/ChargeProductDetailPage"));
 const ChargeCareClaimsPage = lazy(() => import("./pages/ChargeCareClaimsPage"));
+const ChargeWarrantyVerifyPage = lazy(() => import("./pages/ChargeWarrantyVerifyPage"));
 const StaffUIAuditPage = lazy(() => import("./staff/StaffUIAuditPage"));
 const AdminPushBroadcastPage = lazy(() => import("./pages/AdminPushBroadcastPage"));
 const AdminAnalyticsPage = lazy(() => import("./pages/AdminAnalyticsPage"));
@@ -861,6 +862,8 @@ function AppContent() {
         return (isGuest && !isDemoMode)
           ? <HomePage {...homeProps} />
           : <ChargeCareClaimsPage registrationId={routeParams.registration_id} claimId={routeParams.claim_id} onBack={() => handleNavigate("/charge-app")} onNavigate={handleNavigate} />;
+      case "/charge-app/warranty-verify":
+        return <ChargeWarrantyVerifyPage registrationId={routeParams.registration_id} signature={routeParams.sig} onNavigate={handleNavigate} />;
       case "/selfcheckout":
         return (isGuest && !isDemoMode) ? <HomePage {...homeProps} /> : <SelfCheckoutPage onBack={() => handleNavigate("/")} navState={navState} />;
       case "/admin/old":
